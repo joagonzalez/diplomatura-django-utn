6df0e405c16f74da4179bc266b4847100666aef9
Joaquin Gonzalez <joagonzalez@gmail.com>
2023-06-12 12:07:55 -0300

    fix migrations and add category products schema

--- observability_workflows/dashboards/models.py
+++ observability_workflows/dashboards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    
+    def __str__(self):
+        return '%s' % self.name
+
+class Product(models.Model):
+    product = models.CharField(max_length=200)
+    publish_date = models.DateTimeField('Publish Date')
+    image = models.ImageField(upload_to='producto/%Y/%m/%d', blank=True, null=True)
+    category = models.ManyToManyField(Category)
+    
+    def __str__(self):
+        return self.product + ' ' + str(self.publish_date)

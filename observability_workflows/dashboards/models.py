9733a413f0cdb4375d8e8a7548aebb222ba6aa8e
Joaquin Gonzalez <joagonzalez@gmail.com>
2023-06-13 12:16:29 -0300

    admin panel styles

--- observability_workflows/dashboards/models.py
+++ observability_workflows/dashboards/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.html import format_html
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -8,10 +11,30 @@ class Category(models.Model):
         return '%s' % self.name
 
 class Product(models.Model):
+    # descriptors
+    draft = "Draft"
+    published = "Published"
+    retired = "Retired"
+    
+    PRODUCT_APPROVAL = (
+        (draft, "Draft"),
+        (published, "Published"),
+        (retired, "Retired")
+    )
+    
+    status = models.CharField(max_length=10, choices=PRODUCT_APPROVAL, default=draft)
     product = models.CharField(max_length=200)
     publish_date = models.DateTimeField('Publish Date')
     image = models.ImageField(upload_to='producto/%Y/%m/%d', blank=True, null=True)
-    category = models.ManyToManyField(Category)
+    category = models.ForeignKey(Category, blank=False, null=True, on_delete=models.CASCADE)
+    
+    def product_status(self):
+        if self.status == 'Retired':
+            return format_html("<span style='color: #f00;'>{}</span>", self.status)
+        if self.status == 'Draft':
+            return format_html("<span style='color: #f0f;'>{}</span>", self.status)
+        if self.status == 'Published':
+            return format_html("<span style='color: #099;'>{}</span>", self.status)
     
     def __str__(self):
         return self.product + ' ' + str(self.publish_date)

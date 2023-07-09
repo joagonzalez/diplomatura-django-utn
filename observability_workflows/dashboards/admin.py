0354c6fc95361df44681d95fc908c27a604ea511
Joaquin Gonzalez <joagonzalez@gmail.com>
2023-07-09 20:15:13 -0300

    add sidebar and external dashboard integration

--- observability_workflows/dashboards/admin.py
+++ observability_workflows/dashboards/admin.py
@@ -1,33 +1,15 @@
 from django.contrib import admin
-from dashboards.models import Product, Category
+from dashboards.models import Dashboards, Group
 
 
 
-class PorductInline(admin.TabularInline):
-    model = Product
-    extra = 0
+@admin.register(Dashboards)
+class DashboardsAdmin(admin.ModelAdmin):
+    pass
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    inlines = [PorductInline]
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("Relacion", {"fields": ["category"]}),
-        ("Datos Generales", {"fields": ["publish_date", "product", "image", "status"]})
-    ]
-    
-    list_display = ["product", "publish_date", "product_status", "image", "upper_case_name"]
-    ordering = ['-publish_date']
-    list_filter = ['product', 'publish_date']
-    search_fields = ['product', 'product_status']
-    list_display_links = ['product', 'product_status']
-    
-    @admin.display(description='Full Name')
-    def upper_case_name(self, obj):
-        return ("%s %s" % (obj.product, obj.status)).upper()
-    
+@admin.register(Group)
+class GroupsAdmin(admin.ModelAdmin):
+    pass    
 
 # Register your models here.
 #admin.site.register(Product, ProductAdmin)
--- observability_workflows/dashboards/models.py
+++ observability_workflows/dashboards/models.py
@@ -38,3 +38,38 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product + ' ' + str(self.publish_date)
+    
+class Dashboards(models.Model):
+    # descriptors
+    draft = "Draft"
+    published = "Published"
+    retired = "Retired"
+    custom = "Custom"
+    external = "External"
+    
+    DASHBOARD_STATUS = (
+        (draft, "Draft"),
+        (published, "Published"),
+        (retired, "Retired")
+    )
+    
+    DASHBOARD_TYPE = (
+        (custom, "Custom"),
+        (external, "External"),
+    )
+    
+    status = models.CharField(max_length=50, choices=DASHBOARD_STATUS, default=draft)
+    dashboard_type = models.CharField(max_length=50, choices=DASHBOARD_TYPE, default=custom)
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    published_date = models.DateTimeField('Published Date')
+
+        
+    def __str__(self):
+        return self.name + ' ' + str(self.dashboard_type)
+    
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    dashboards = models.ManyToManyField(Dashboards)

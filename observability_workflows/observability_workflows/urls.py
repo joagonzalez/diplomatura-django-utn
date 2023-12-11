dc2d7c36d5bcfbf47e57370c8572b503a2a3d11b
Joaquin Gonzalez <joagonzalez@gmail.com>
2023-12-11 13:03:06 -0300

    add sitemaps

--- observability_workflows/observability_workflows/urls.py
+++ observability_workflows/observability_workflows/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from observability_workflows import views
 from django.conf.urls.static import static
 from django.urls import path, include, re_path
+from tienda.sitemap import TiendaSiteMap
+from dashboards.sitemap import DashboardsSiteMap
+from django.contrib.sitemaps.views import sitemap
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
+sitemaps = {
+    'dashboards': DashboardsSiteMap,
+    'tienda': TiendaSiteMap,
+}
 
 urlpatterns = [
     # path('', view=views.index, name='index'),
@@ -32,6 +40,7 @@ urlpatterns = [
     path('captcha/', include('captcha.urls')),
     path('contacto/', include('contacto.urls')),
     path('tienda/', include('tienda.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     # path('accounts/', include('registration.backends.default.urls'), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
